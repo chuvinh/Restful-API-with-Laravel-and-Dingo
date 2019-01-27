4518557b2a0147307c991ec9fc504fe229a852b3
code-architect <codearchitectdeveloper@gmail.com>
2019-01-27 06:52:00 +0530

    created a console command to execute the services from cli, TODO: Can create a task scheduler to run a cron job or event or something like taht

--- app/Models/WeatherStatus.php
+++ app/Models/WeatherStatus.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class WeatherStatus extends Model
 {
+    public $table = "weather_status";
     public function city()
     {
         return $this->belongsTo(City::class);
