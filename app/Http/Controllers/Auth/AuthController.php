dfa1965241a9310564999d8c404e8616c66161ea
code-architect <codearchitectdeveloper@gmail.com>
2019-01-31 05:42:52 +0530

    Created auth controller, Remember: please run "php artisan jwt:secret" before testing in Postman or it will throw error

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Http\Transformers\UserTransformer;
 use App\Services\UserService;
-use Illuminate\Http\Request;
+use Dingo\Api\Http\Request;
+use Dingo\Api\Http\Response;
 use App\Http\Controllers\Controller;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 use Tymon\JWTAuth\JWTAuth;
 
 class AuthController extends Controller
@@ -15,8 +18,46 @@ class AuthController extends Controller
     public function __construct(UserService $userService, JWTAuth $authService)
     {
         $this->userService = $userService;
-        $this->authService = $userService;
+        $this->authService = $authService;
+    }
+
+    /**
+     * Get login token
+     * @param Request $request
+     * @return mixed
+     */
+    public function login(Request $request)
+    {
+        $token = $this->authService->attempt(
+            $request->only(['email', 'password'])
+        );
+
+        // if we didn't get our token throw error
+        if(!$token)
+        {
+            throw new UnauthorizedHttpException('jwt');
+        }
+
+        // if we get token return it
+        return $this->success([
+            'token' => $token
+        ]);
+    }
+
+    public function refreshToken()
+    {
+        return $this->authService->parseToken()->refresh();
     }
 
 
+    public function getUser(Request $request)
+    {
+        return $this->response->item($request->user(), new UserTransformer());
+    }
+
+    public function register(Request $request)
+    {
+        $user = $this->userService->create($request->all());
+        return $this->response->item($request->user(), new UserTransformer());
+    }
 }
