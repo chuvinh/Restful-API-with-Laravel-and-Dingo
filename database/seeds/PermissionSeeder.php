c8adc2d1ba2e19ecc0014b910d224d2f4b71b009
code-architect <codearchitectdeveloper@gmail.com>
2019-02-03 04:18:11 +0530

    fixed a database seeding problem

--- database/seeds/PermissionSeeder.php
+++ database/seeds/PermissionSeeder.php
@@ -11,6 +11,20 @@ class PermissionSeeder extends Seeder
      */
     public function run()
     {
-        //
+        \Illuminate\Database\Eloquent\Model::unguard();
+        // name, display_name, description
+        $permissions = [
+            [ 'query-temperature', 'Query the temperature', 'User can query the temperature from the database' ],
+        ];
+        foreach ($permissions as $permission) {
+            $permissionInstance = \App\Models\Auth\Permission::firstOrNew([
+                'name' => $permission[0],
+            ]);
+            $permissionInstance->display_name = $permission[1];
+            $permissionInstance->description = $permission[2];
+            $permissionInstance->save();
+        }
+
+        \Illuminate\Database\Eloquent\Model::reguard();
     }
 }
--- database/seeds/RoleSeeder.php
+++ database/seeds/RoleSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\Auth\Permission;
 
 class RoleSeeder extends Seeder
 {
@@ -11,6 +12,43 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
-        //
+        \Illuminate\Database\Eloquent\Model::unguard();
+
+        // name, display_name, description, [permission1, permission2, ...]
+        $roles = [
+            [ 'root', 'root', 'This role gives you all permissions', ['*'] ],
+            [ 'api-user', 'Api user', '', ['query-temperature'] ],
+        ];
+
+        foreach ($roles as $role) {
+            $roleEntry = \App\Models\Auth\Role::firstOrNew([
+                'name' => $role[0],
+            ]);
+
+            $roleEntry->display_name = $role[1];
+            $roleEntry->description = $role[2];
+            $roleEntry->save();
+
+            if (array_key_exists(3, $role)) {
+                foreach ($role[3] as $permission) {
+                    if ($permission == '*') {
+                        $roleEntry->syncPermissions(Permission::all());
+                        break;
+                    }
+
+                    $permission = Permission::where(['name' => $permission])->first();
+                    if ($permission) {
+                        $roleEntry->syncPermissions([$permission]);
+                        break;
+                    }
+
+                    $message = "Unknown permission: " . $permission;
+                    Log::error($message);
+                    echo $message . PHP_EOL;
+                }
+            }
+        }
+
+        \Illuminate\Database\Eloquent\Model::reguard();
     }
 }
